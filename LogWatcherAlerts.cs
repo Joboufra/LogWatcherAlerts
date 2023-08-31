6a4de8918079a356cce8237f7549802ac88ad9cf
Joboufra <joboufra@gmail.com>
2023-08-31 20:13:26 +0200

    first commit

---
+++ LogWatcherAlerts.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+class LogWatcherAlerts
+{
+    private static object lockObject = new object();
+    private static long lastPosition = 0;
+    private static IConfiguration _configuration;
+
+    private static void SaveLastPosition() => File.WriteAllText("lastPosition.txt", lastPosition.ToString());
+    private static void LogMessage(string message) => Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}");
+
+    private static void LoadLastPosition()
+    {
+        if (File.Exists("lastPosition.txt"))
+        {
+            string content = File.ReadAllText("lastPosition.txt");
+            long.TryParse(content, out lastPosition);
+        }
+    }
+
+    static void Main()
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        LoadLastPosition();
+
+        string path = _configuration.GetSection("AppSettings")["LogFilePath"];
+        FileSystemWatcher watcher = new FileSystemWatcher(Path.GetDirectoryName(path), Path.GetFileName(path));
+
+        watcher.Changed += OnChanged;
+        watcher.EnableRaisingEvents = true;
+        Console.WriteLine(":: LogWatcherAlerts -> Jira Tickets ::");
+        Console.WriteLine(":: Jose Boullosa @ 2023 ::");
+        Console.WriteLine("");
+        LogMessage("Programa iniciado. Presiona enter para salir");
+        Console.ReadLine();
+
+        SaveLastPosition();
+    }
+
+    private static async void OnChanged(object sender, FileSystemEventArgs e)
+    {
+        List<string> alertMessages = new List<string>();
+
+        lock (lockObject)
+        {
+            using (FileStream stream = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                stream.Seek(lastPosition, SeekOrigin.Begin);
+
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Contains("[ALERTA]")) alertMessages.Add(line.Replace("[ALERTA]", ""));
+                    }
+                    lastPosition = stream.Position;
+                }
+            }
+        }
+        SaveLastPosition();
+
+        foreach (var alert in alertMessages)
+        {
+            await ProcessAlert(alert);
+        }
+    }
+
+    private static async Task ProcessAlert(string alert)
+    {
+        string apiKey = _configuration.GetSection("AppSettings")["ApiKey"];
+        string[] messages = alert.Split(';');
+
+        // JSON para Jira
+        JObject jiraData = new JObject(
+            new JProperty("fields", new JObject(
+                new JProperty("project", new JObject(new JProperty("key", "AL"))),
+                new JProperty("summary", messages[0]),
+                new JProperty("description", messages[1]),
+                new JProperty("issuetype", new JObject(new JProperty("name", "Alerta"))))));
+
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes($"joboufra@proton.me:{apiKey}")));
+
+            var response = await client.PostAsync("https://joboufra.atlassian.net/rest/api/2/issue",
+                new StringContent(jiraData.ToString(), Encoding.UTF8, "application/json"));
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                JObject jsonResponse = JObject.Parse(responseBody);
+                string ticketKey = jsonResponse["key"].ToString();
+                string ticketUrl = jsonResponse["self"].ToString();
+                LogMessage($"Ticket creado: {ticketKey}. Enlace: {ticketUrl}");
+            }
+            else
+            {
+                LogMessage("Error al crear ticket");
+            }
+
+        }
+    }
+}
